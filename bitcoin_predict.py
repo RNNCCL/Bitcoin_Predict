edb2e0c68e387a9b912f0a471c3bc3429fb75f6f
yuk1h1ra <koheishoji913@gmail.com>
2017-11-09 15:52:22 +0900

    add periods

--- bitcoin_predict.py
+++ bitcoin_predict.py
@@ -4,8 +4,8 @@ import requests
 import pandas as pd
 
 def get_data(time=300):
-    URL = 'https://api.cryptowat.ch/markets/bitflyer/btcjpy/ohlc'
-    response = requests.get(URL).json()
+    URL = 'https://api.cryptowat.ch/markets/bitflyer/btcjpy/ohlc?periods='
+    response = requests.get(URL, time).json()
     df_order = pd.DataFrame(response['result'][str(time)])
     df_order.columns = ['CloseTime', 'OpenPrice', 'HighPrice', 'LowPrice', 'ClosePrice', 'Volume']
     print(df_order.head(20))
