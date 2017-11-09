6e802e81d186744e2049ebc13bb25c2c98a51529
yuk1h1ra <koheishoji913@gmail.com>
2017-11-09 15:30:48 +0900

    change response to dataframe

--- bitcoin_predict.py
+++ bitcoin_predict.py
@@ -3,10 +3,12 @@
 import requests
 import pandas as pd
 
-def get_data():
+def get_data(time=300):
     URL = 'https://api.cryptowat.ch/markets/bitflyer/btcjpy/ohlc'
-    response = requests.get(URL)
-    print(response)
+    response = requests.get(URL).json()
+    df_order = pd.DataFrame(response['result'][str(time)])
+    df_order.columns = ['CloseTime', 'OpenPrice', 'HighPrice', 'LowPrice', 'ClosePrice', 'Volume']
+    print(df_order.head(20))
 
 def main():
     get_data()
