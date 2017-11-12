e9625702cbacd188c972859b0a2fb2e03d207e3c
yuk1h1ra <koheishoji913@gmail.com>
2017-11-12 11:49:57 +0900

    add get data

---
+++ LSTM.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+'''cryptowatchAPIにて、取得したデータをディープラーニング（）'''
+
+def get_data(time=300):
+    URL = 'https://api.cryptowat.ch/markets/bitflyer/btcjpy/ohlc?periods='
+    response = requests.get(URL, time).json()
+    df_order = pd.DataFrame(response['result'][str(time)])
+    df_order.columns = ['CloseTime', 'OpenPrice', 'HighPrice', 'LowPrice', 'ClosePrice', 'Volume']
+    return df_order
+
+def main():
+    # 変数
+    TIME = 300
+
+    df_order = get_data(time=TIME)
+
+if __name__ == '__main__':
+    main()
